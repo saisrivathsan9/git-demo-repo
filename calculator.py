630c426441072e5b6ad9fcc7dc3bbc29d53e7251
saisrivathsan9 <58337977+saisrivathsan9@users.noreply.github.com>
2025-10-17 21:47:39 -0700

    Added subtraction, multiplication and division function

--- calculator.py
+++ calculator.py
@@ -4,6 +4,17 @@
 def add(a, b):
     return a + b
 
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a,b):
+    return a/b
 
 if __name__ == '__main__':
     print(f"Adding 5 and 3: {add(5, 3)}")
+    print(f"Subtracting 10 from 7: {subtract(10, 7)}")
+    print(f"Multiplying 4 by 6: {multiply(4, 6)}")
+    print(f"Dividing 12 by 6: {multiply(12, 6)}")
