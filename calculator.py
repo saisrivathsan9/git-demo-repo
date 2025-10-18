350d1a9f3e9fa07d66e119e64066089ed9d660dc
saisrivathsan9 <58337977+saisrivathsan9@users.noreply.github.com>
2025-10-17 21:41:13 -0700

    Added addition function

--- calculator.py
+++ calculator.py
@@ -1,3 +1,9 @@
 # calculator.py
 # This is a simple tutorial to demonstrate Git version control.
 
+def add(a, b):
+    return a + b
+
+
+if __name__ == '__main__':
+    print(f"Adding 5 and 3: {add(5, 3)}")
